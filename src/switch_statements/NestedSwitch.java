5859e9f72eb790641101f87d7c45c5daee416637
btazhibaev <baktybektazhibaev@gmail.com>
2021-05-24 21:31:13 -0500

    Nested switch and combined with if/switch

--- src/switch_statements/NestedSwitch.java
+++ src/switch_statements/NestedSwitch.java
@@ -8,21 +8,45 @@ public class NestedSwitch {
 
         System.out.println("Are you invited to this wedding? Please type 'Yes' or 'No'");
         String status = new Scanner(System.in).next().toLowerCase();// all the characters will be converted to lower case
-        System.out.println(status);
 
         System.out.println("Please enter your name: ");
         String name = new Scanner(System.in).next().toUpperCase(); // all the characters will be converted to upper case
-        System.out.println(name);
+
+        System.out.println("Please enter your age: ");
+        int age = new Scanner(System.in).nextInt();
 
         switch (status){
             case "yes":
-                switch (name){
-                    case "ELINA": case "EDISA": case "AIZADA": case "ANNA":
+                switch (name) {
+                    case "ELINA", "EDISA", "AIZADA", "ANNA":
                         System.out.println("Welcome to the amazing wedding!");
-                        break;
-                    default:
-                        System.out.println("You are not on the list, please contact admin!");
-                        break;
+                        if(age >= 21){
+                            System.out.println("You can enjoy this evening by the bar!");
+
+                            System.out.println("Please enter the type of beverage that contains alcohol. Ex: beer, wine, shots.");
+                            String beverageType = new Scanner(System.in).next();
+                            beverageType = beverageType.toLowerCase();
+
+                            switch (beverageType){
+                                case "beer":
+                                    System.out.println("Good choice! Here is list of beers: Stella artois, Nashe, Arpa, Heineken, Bud, Miller");
+                                    break;
+                                case "wine":
+                                    System.out.println("Good choice! Chardonnay, Black Eyes, Red rose");
+                                    break;
+                                case "shots":
+                                    System.out.println("Way to go! Tequila, Vodka, Kamikadze");
+                                    break;
+                                default:
+                                    System.out.println("Sorry. We only have limited menu at this bar.");
+                            }
+                            break;
+                        }else {
+                            System.out.println("Please get yourself a juice!");
+                        }
+                    break;
+                default:
+                    System.out.println("You are not on the list, please contact admin!");
                 }
                 break;
             case "no":
@@ -32,5 +56,6 @@ public class NestedSwitch {
             default:
                 System.out.println("VIP");
         }
+        System.out.println("Get home safe! Watch your step!");
     }
 }
